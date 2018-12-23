74e17f27b5d609bc8f419b56bc1c0eb367c29564
sidythakur <34541410+sidythakur@users.noreply.github.com>
2018-12-23 14:59:06 +0530

    Update index.js

--- a4/index.js
+++ a4/index.js
@@ -10,7 +10,7 @@ const examples = {
   'example1':
       'Alice was beginning to get very tired of sitting by her sister on the bank.',
   'example2':
-      'Buda-Pesth seems a wonderful place.',
+      'Left Munich at 8:35 P. M., on 1st May, arriving at Vienna early next morning.',
   'example3':
       'Scepticism was as much the result of knowledge, as knowledge is of scepticism.'      
 };
